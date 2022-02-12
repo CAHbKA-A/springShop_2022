2cbcc638e2ff539dd873ccdeae3e852940c1f2cb
Student <123@12d.dd>
2022-02-12 18:57:23 +0800

    add кладем json в скоуп в в иде переменной productList. в хтмл в таблицу распаквываем productList

--- src/main/resources/static/products.js
+++ src/main/resources/static/products.js
@@ -1,10 +1,22 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     console.log('start');
     $http.get('http://localhost:8189/shop/api/v1/products').then(function (response){
-        console.log(response);
+       //вывод в кносоль json
+        // console.log(response.data);
+        //переменная. кладем в нее json .  let - объявление переменной
+        //let products = response.data;
+        //console.log(products[0].title)
+        //создаем переменную productsList и кладем в скоуп, чтобы потом достать на html. в переменную кладем json
+        $scope.productsList = response.data;
+
     })
 
 
 
+    // $scope.loadProducts = function () {
+    //     $http.get('http://localhost:8189/shop/api/v1/products').then(function (response) {
+    //         $scope.productsList = response.data;
+    //     });
+    // }
 
 });
