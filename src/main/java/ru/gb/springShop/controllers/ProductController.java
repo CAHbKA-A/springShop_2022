3f59367e3304d78229ed47a75e2b1cb33c85ca67
Student <123@12d.dd>
2022-02-16 23:22:29 +0800

    add обраюотка исключений (not found)

--- src/main/java/ru/gb/springShop/controllers/ProductController.java
+++ src/main/java/ru/gb/springShop/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package ru.gb.springShop.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.springShop.entities.Product;
+import ru.gb.springShop.exceptions.AppError;
+import ru.gb.springShop.exceptions.ResourceNotFoundException;
 import ru.gb.springShop.services.CartService;
 import ru.gb.springShop.services.ProductService;
 
@@ -25,18 +29,48 @@ public class ProductController {
         return productService.findAll();
     }
 
+
+
+
+
+    /*объемный вариант*/
     //выдергиваем  объект по id
+/*      @GetMapping("/{id}")
+    //возварщать будем респонс записль либо с ошибкой , либо с продуктом
+  public ResponseEntity<?> findProductById(@PathVariable Long id) {
+        //обработка ошибок
+        Optional<Product> product = productService.findById(id);
+        //Если не найден продукт
+        if (!product.isPresent()) {
+            //ответка от апи в сторону js.  упаковываем в json
+            // HttpStatus - это enum, который содержит все статусы
+            ResponseEntity<AppError> error = new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), "Продукт не найден, id: " + id), HttpStatus.NOT_FOUND);//последний аргумент - стаким статускодом прилетит ответ(поимо json'a
+            return error;
+        }
+        return new ResponseEntity<>(product.get(),HttpStatus.OK);
+    }
+*/
+    /*вариант поменьше*/
+
     @GetMapping("/{id}")
     public Product findProductById(@PathVariable Long id) {
-        return productService.findById(id).get();
+
+        return productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Продукт не найден, id: " + id));//orElseThrow()-вернет продукт ,е сли есть, а если нет, исключение
     }
+
+    // создаем метод, перехватывающий исключения
+    @ExceptionHandler //чтобы создался этот эксепшн хендлен и перехватывал исключения
+    public ResponseEntity<AppError> exeptionHandler(ResourceNotFoundException e) {
+        //если поймали исключение
+        return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
 
     //удаляем объект по id
     @DeleteMapping("/{id}")
     void deleteProductById(@PathVariable Long id) {
         productService.deleteById(id);
     }
-
 
 
 }
