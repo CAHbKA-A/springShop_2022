e265d3d2fe8457509cf10fd955ca0785d574fd65
Student <123@12d.dd>
2022-03-07 03:17:17 +0800

    add value url . soap пока неработет

--- cart-service/src/main/java/ru/gb/springShop/carts/intrgrations/ProductServiceIntegration.java
+++ cart-service/src/main/java/ru/gb/springShop/carts/intrgrations/ProductServiceIntegration.java
@@ -2,18 +2,24 @@ package ru.gb.springShop.carts.intrgrations;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.gb.springShop.api.ProductDto;
 
-
 import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class ProductServiceIntegration {
+
+    @Value("${integration.url}")
+    private String URL;
+
+
     private final RestTemplate restTemplate;
 
     public Optional<ProductDto> getProductById(Long id) {
-        return Optional.ofNullable(restTemplate.getForObject("http://localhost:8189/shop/api/v1/products/" + id, ProductDto.class));
+        return Optional.ofNullable(restTemplate.getForObject(URL + "/shop/api/v1/products/" + id, ProductDto.class));
     }
 }
--- core-service/src/main/java/ru/gb/springShop/core/intrgrations/CartServiceIntegration.java
+++ core-service/src/main/java/ru/gb/springShop/core/intrgrations/CartServiceIntegration.java
@@ -2,24 +2,28 @@ package ru.gb.springShop.core.intrgrations;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.gb.springShop.api.CartDto;
-import ru.gb.springShop.api.ProductDto;
 
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class CartServiceIntegration {
+    @Value("${integration.url}")
+    private String URL;
+
+
     private final RestTemplate restTemplate;
 
     public Optional<CartDto> getCart() {
-        return Optional.ofNullable(restTemplate.getForObject("http://localhost:8181/shop-carts/api/v1/cart" , CartDto.class));
+        return Optional.ofNullable(restTemplate.getForObject(URL+"/shop-carts/api/v1/cart" , CartDto.class));
     }
 
     public void  clear() {
-       restTemplate.delete("http://localhost:8181/shop-carts/api/v1/cart/clear" );
+       restTemplate.delete(URL+"/shop-carts/api/v1/cart/clear" );
     }
 
 }
