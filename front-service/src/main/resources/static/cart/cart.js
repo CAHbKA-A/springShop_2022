7ad6609069aa9ae1154a968927cb31f408efbe24
Student <123@12d.dd>
2022-03-28 23:29:42 +0800

    разделение фронта на страницы

---
+++ front-service/src/main/resources/static/cart/cart.js
@@ -0,0 +1,49 @@
+angular.module('market').controller('cartController', function ($scope, $http, $location, $localStorage) {
+    const contextPath = 'http://localhost:5555/cart/';
+    const coreContextPath = 'http://localhost:5555/core/';
+
+    $scope.loadCart = function () {
+        $http.get(contextPath + 'api/v1/cart').then(function (response) {
+            $scope.cart = response.data;
+        });
+    }
+
+    $scope.clearCart = function () {
+        $http.get(contextPath + 'api/v1/cart/clear').then(function (response) {
+            $scope.loadCart();
+        });
+    }
+
+
+    $scope.deleteItemFromCart = function (id) {
+        $http.get(contextPath + 'api/v1/cart/deleteItem/' + id).then(function (response) {
+            $scope.loadCart();
+        });
+    }
+
+
+    //todo сохранение в локальное хранилище браузера телефона и адреса. или подтягивать из бд
+    $scope.createOrder = function () {
+
+        $http.post(coreContextPath+'api/v1/orders', $scope.user).then(function successCallback(response) { //HttpStatus.CREATED)
+            //reload или перейти  на страницу заказов
+            console.log()
+            $scope.loadCart($scope.user);
+
+
+        }, function errorCallback(response) {
+            alert("error. или не ввели адрес телефон или косяк на бэке. пока не разделяю")
+        });
+
+    };
+
+    $scope.setCountItem = function (productId, count) {
+        $http.get(contextPath + 'api/v1/cart/SetCountItem?id=' + productId + '&count=' + count).then(function (response) {
+            $scope.loadCart();
+        });
+    }
+
+
+
+    $scope.loadCart();
+});
---
+++ front-service/src/main/resources/static/store/store.js
@@ -0,0 +1,47 @@
+angular.module('market').controller('storeController', function ($scope, $http, $location, $localStorage) {
+    const contextPath = 'http://localhost:5555/core/';
+    const cartContextPath = 'http://localhost:5555/cart/';
+
+
+
+    $scope.showProductInfo = function (productId) {
+        $http.get(contextPath + 'api/v1/products/' + productId).then(function (response) {
+            alert(response.data.description);
+        });
+    }
+
+
+    $scope.addToCart = function (productId) {
+        $http.get(cartContextPath + 'api/v1/cart/add/' + productId).then(function (response) {
+
+        });
+    }
+
+
+    $scope.loadProducts = function () {
+        $http.get(contextPath + 'api/v1/products').then(function (response) {
+              $scope.productsList = response.data;
+        });
+    }
+
+
+
+    $scope.deleteProductById = function (id) {
+        $http.delete(contextPath + 'api/v1/products' + id).then(function (response) {
+             $scope.loadProducts();
+        });
+    }
+
+
+
+    $scope.filter = function () {
+        console.log("filter");
+        $http.post(contextPath + 'api/v1/products/filter', $scope.filterData).then(function (response) {
+            $scope.productsList = response.data;
+
+        });
+    }
+
+
+    $scope.loadProducts();
+});
